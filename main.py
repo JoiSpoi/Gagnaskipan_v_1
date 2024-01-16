86b4c5fdee79c47fefd017ce500b8d7a495b132b
Spoi_Laptop <johanngunnare@gmail.com>
2024-01-16 14:42:52 +0000

    Created player movement, and the tiles

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from player import Player
 
 player = Player((1, 1))
 tiles = {
-    (1, 1): Tile(['e', 's']),
-    (2, 1): Tile(['w', 'e']),
-    (3, 1): Tile(['w']),
-    (1, 2): Tile(['e']),
-    (2, 2): Tile(['w', 'e', 's']),
-    (3, 2): Tile(['w', 's']),
-    (1, 3): Tile(['e', 's']),
-    (2, 3): Tile(['w', 's']),
+    (1, 1): Tile(["n"]),
+    (2, 1): Tile(["n"]),
+    (3, 1): Tile(["n"]),
+    (1, 2): Tile(["n", "a", "s"]),
+    (2, 2): Tile(["w", "s"]),
+    (3, 2): Tile(["n", "s"]),
+    (1, 3): Tile(["w", "s"]),
+    (2, 3): Tile(["w", "a"]),
 }
 
 def main():
