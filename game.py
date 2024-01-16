091cfd1ba6639cb89bfafc812af84e402768cbbf
bergurh93 <bergurhalldorsson@gmail.com>
2024-01-16 14:34:37 +0000

    added to game.py

--- game.py
+++ game.py
@@ -8,12 +8,17 @@ class Game:
         """Starts a new game, setting player position to 1,1 and coins to 0."""
         self.player = Player((1,1))
         self.player.coins = 0
-        self.end = (3,1)
+        self.end = (3,1) #Can be modified later when multiple levels are implemented.
         return
-    
+
+    def player_input(self):
+        """Receives player input."""
+        p_input = input().lower()
+        if p_input in 
+        
     def run_game(self):
         """Contains main game loop."""
         while self.player.location != self.end:
-            pass
+            print(f"You are at {self.player.location}. You have {self.coins} gold coins.")
+            self.player.move_player()
     
-
