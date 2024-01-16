dec9b4cdd8052e1933784b46a8bac7deff93e13a
Spoi_Laptop <johanngunnare@gmail.com>
2024-01-16 14:00:53 +0000

    Added player and tile class

---
+++ player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self, starting_location) -> None:
+        self.location = starting_location
+
+    def move_player(self):
+        ...
---
+++ tile.py
@@ -0,0 +1,3 @@
+class Tile:
+    def __init__(self, possible_moves: list[str]) -> None:
+        self.possible_moves = possible_moves
