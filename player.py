ea7947f49ac55a510d526a67dd701481ba10f0b6
Spoi_Laptop <johanngunnare@gmail.com>
2024-01-16 14:30:29 +0000

    Worked on classes, and started creating the main file

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 class Player:
-    def __init__(self, starting_location) -> None:
+    def __init__(self, starting_location: tuple[int]) -> None:
         self.location = starting_location
 
     def move_player(self, movement: str):
--- tile.py
+++ tile.py
@@ -4,4 +4,7 @@ class Tile:
 
     def is_valid_move(self, move: str) -> bool:
         """Returns True if the move is valid."""
+        if not isinstance(move, str):
+            raise TypeError(f"expected move argument to be of type str, but got {type(move)}")
+
         return move.lower() in self.possible_moves
